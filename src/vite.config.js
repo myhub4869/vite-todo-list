72e21c9e6c2c39eb7e97ea95d7b1a7b947d16a5f
miyahara <kenmiya.sns@gmail.com>
2024-09-24 15:35:51 +0900

    [feat]packageの追加

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,18 +5,27 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    server: { 
+    server: {
         host: '0.0.0.0',
         hmr: {
-            host: 'localhost',
+            host: 'localhost'
         },
-    }, 
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
