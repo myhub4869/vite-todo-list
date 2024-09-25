f53eb26d4ca698b13baecf8a8c7b449a6b01187b
miyahara <kenmiya.sns@gmail.com>
2024-09-25 10:12:16 +0900

    [feat]viewのoverwrap関数を追加

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function page($view = null, $data = [], $mergeData = [])
+    {
+        return view("page/{$view}", $data, $mergeData);
+    }
 }
