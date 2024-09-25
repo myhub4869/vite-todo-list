883f0073b92ac045791dc57047ceda442c207810
miyahara <kenmiya.sns@gmail.com>
2024-09-25 10:12:02 +0900

    [feat]todoのマイグレーションを追加

---
+++ src/database/migrations/2024_09_25_010930_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id")->nullable(false)->comment("ユーザID");
+
+            $table->string("title", 512)->nullable(false)->comment("Todoタイトル");
+            $table->boolean('completed')->default(false)->comment("完了フラグ");
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->comment("Todo");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
